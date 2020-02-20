dbb02a20ad1fa5f09a846512d6292a538743eb38
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-02-20 11:01:14 +0100

    test(react): install jest + dependencies and set it up (refs #7)

---
+++ ai-kit-auth/src/components/Login.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export const LoginView: FC = () => (
+  <div>
+    Login View
+  </div>
+);
+
+export default LoginView;
--- ai-kit-auth/src/index.ts
+++ ai-kit-auth/src/index.ts
@@ -1,1 +1,2 @@
-export default {};
+
+export { LoginView } from './components/Login';
