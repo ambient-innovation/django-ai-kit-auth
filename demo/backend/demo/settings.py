360404e79a11c9a20184819fdaa896950542f20d
matthias.tellen@ambient-innovation.com <e1993DCnCPCq6MtpuJwb>
2020-02-21 08:33:03 +0100

    #6 Kleinigkeiten nach Code Review

--- demo/backend/demo/settings.py
+++ demo/backend/demo/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = (
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
-    'rest_framework.authtoken',
-    'rest_auth',
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_auth",
     "django-ai-kit-auth",
 )
 
--- django-ai-kit-auth/account/urls.py
+++ django-ai-kit-auth/account/urls.py
@@ -4,6 +4,4 @@ from django.urls import include, re_path
 # urls. Later when we do the account reset stuff it makes sense to not use
 # the rest_auth endpoints in the target app directly because we can do the
 # configuration here that is a little bit fiddly
-urlpatterns = [
-    re_path(r'', include('rest_auth.urls'))
-]
+urlpatterns = [re_path(r"", include("rest_auth.urls"))]
