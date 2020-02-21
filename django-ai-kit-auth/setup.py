86ce2157dade6572a6dab49d9398da2b1a45c51e
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-02-21 16:41:36 +0100

    test(django): mention test runner in setup.py (refs #6)

--- django-ai-kit-auth/setup.py
+++ django-ai-kit-auth/setup.py
@@ -6,4 +6,9 @@ with open("account/__init__.py", "r") as fd:
         r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
     ).group(1)
 
-setup(version=version, install_requires=["Django>=2.0.0"], tests_require=[])
+setup(
+    version=version,
+    install_requires=["Django>=2.0.0"],
+    tests_require=[],
+    test_suite="runtests.runtests",
+)
