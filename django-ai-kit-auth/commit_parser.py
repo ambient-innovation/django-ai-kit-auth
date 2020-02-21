93ddd591c7fc40d916b30fecfa7f513fdfa832ac
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-02-21 13:51:02 +0100

    ci(django): integrate python-semantic-release (refs #6)

---
+++ django-ai-kit-auth/commit_parser.py
@@ -0,0 +1,10 @@
+from semantic_parser.history import angular_parser
+
+
+def django_parser(commit_msg):
+    level, change_type, scope, content = angular_parser(commit_msg)
+
+    if 'django' not in scope:
+        return 0, 'chore', scope, content
+
+    return level, change_type, scope, content
--- django-ai-kit-auth/setup.py
+++ django-ai-kit-auth/setup.py
@@ -1,3 +1,13 @@
+import re
 from setuptools import setup
 
-setup()
+with open('account/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
+setup(
+    version=version
+)
