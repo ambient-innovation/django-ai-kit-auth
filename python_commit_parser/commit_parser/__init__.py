5a813738ca55abbd68b5ae27cf9a13be94ee1d53
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-02-26 12:36:47 +0100

    ci(django): patch get_repository_owner_and_name (refs #6)

--- python_commit_parser/commit_parser/__init__.py
+++ python_commit_parser/commit_parser/__init__.py
@@ -1,4 +1,14 @@
 from semantic_release.history.parser_angular import parse_commit_message
+from unittest.mock import patch
+
+def get_owner_and_name():
+    return 'ai/ai.kit', 'authentication'
+
+
+# semantic_release does not recognize owner names with dots in them,
+# so we need to patch the function which obtains it until it is fixed
+# in python-semantic-release
+patch('semantic_release.cli.get_repository_owner_and_name', get_owner_and_name).start()
 
 
 def django_parser(commit_msg):
