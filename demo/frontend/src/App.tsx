fecbb40aac2525da982214137468497f02b3ffb8
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-03-03 14:11:41 +0100

    feat(demo): add browser and routes in demo (refs #1)

--- demo/frontend/src/App.tsx
+++ demo/frontend/src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { UserStore, LoginView } from 'ai-kit-auth';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import {
+  UserStore, LoginView, LoginRoute, ProtectedRoute,
+} from 'ai-kit-auth';
 
 const App: React.FC = () => (
   <UserStore
     apiUrl="http://localhost:8000/api/v1/"
   >
-    <div className="App">
-    AI-KIT AUTH Demo Project
+    <BrowserRouter>
+      <Switch>
+        <LoginRoute exact path="/login" component={LoginView} />
+        <ProtectedRoute exact path="/">
+          <div>
+            Django Test App
+          </div>
+        </ProtectedRoute>
+      </Switch>
       <LoginView />
-    </div>
+    </BrowserRouter>
   </UserStore>
 );
 
