f142470e06778afd7186543c19fac68626e4e07d
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-03-03 13:16:27 +0100

    style(react): use Grid for width of LoginView (refs #1)

--- react-lib/src/components/LoginView.tsx
+++ react-lib/src/components/LoginView.tsx
@@ -1,8 +1,31 @@
+import Grid from '@material-ui/core/Grid';
 import React, { FC } from 'react';
+import { createStyles, Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { LoginForm } from './LoginForm';
 
-export const LoginView: FC = () => (
-  <div>
-    <LoginForm />
-  </div>
-);
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  loginView: {
+    height: '100vh',
+  },
+}));
+
+export enum Identifier {
+  Username = 1,
+  Email= 2,
+  UsernameOrEmail = 3,
+}
+export const LoginView: FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.loginView}>
+      <Grid item xs={4}>
+        Image
+      </Grid>
+      <Grid item xs={6}>
+        <LoginForm />
+      </Grid>
+    </Grid>
+  );
+};
