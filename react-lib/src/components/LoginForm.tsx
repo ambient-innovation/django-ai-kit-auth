0605ba49e3fd66e89d0933c5b53c0fc599d08547
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-03-03 11:59:12 +0100

    feat(react): make password revealable (refs #1)

--- react-lib/src/components/LoginForm.tsx
+++ react-lib/src/components/LoginForm.tsx
@@ -1,19 +1,29 @@
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import React, { FC, useState } from 'react';
-import { type } from 'os';
-import { Paper } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
 import { useUserStore } from '../store/UserStore';
 import { en as strings } from '../internationalization';
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles((theme: Theme) => createStyles({
   loginForm: {
     paddingTop: 35,
     paddingLeft: 47,
     paddingRight: 30,
     paddingBottom: 36,
     boxShadow: '0 1px 36px 0 rgba(211, 211, 211, 0.5)',
+  },
+  loginTitle: {
+    marginBottom: 35,
+
+  },
+  inputField: {
+    marginBottom: 30,
   },
 }));
 export enum Identifier {
@@ -35,11 +45,18 @@ export const makeLoginForm: (options: LoginFormOptions) => FC = ({
   const { login } = useUserStore();
   const [userIdentifier, setUserIdentifier] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <Paper
       className={classes.loginForm}
     >
+      <Typography
+        className={classes.loginTitle}
+        variant="h2"
+      >
+        {strings.LoginForm.Login}
+      </Typography>
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -47,7 +64,9 @@ export const makeLoginForm: (options: LoginFormOptions) => FC = ({
         }}
       >
         <TextField
+          className={classes.inputField}
           autoFocus
+          fullWidth
           id="userIdentifier"
           label={strings.LoginForm[Identifier[identifier] as IdentifierType]}
           variant="outlined"
@@ -57,21 +76,39 @@ export const makeLoginForm: (options: LoginFormOptions) => FC = ({
           }}
         />
         <TextField
+          className={classes.inputField}
+          fullWidth
           id="password"
           label={strings.LoginForm.Password}
           variant="outlined"
           value={password}
-          type="password"
+          type={showPassword ? 'text' : 'password'}
           onChange={(event) => {
             setPassword(event.target.value);
           }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPassword(!showPassword)}
+                  onMouseDown={(e) => e.preventDefault()}
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
+
         <Button
           type="submit"
-          title={strings.LoginForm.Login}
+          title="Anmelden"
           variant="contained"
           color="primary"
-        />
+        >
+          {strings.LoginForm.Login}
+        </Button>
       </form>
     </Paper>
   );
