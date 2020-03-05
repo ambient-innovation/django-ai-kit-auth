1a02ea4408b1fc3125e93bccc5be8772da689dff
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-03-05 16:25:03 +0100

    ci(django): guard against None scope (refs #4)

--- python_commit_parser/commit_parser/__init__.py
+++ python_commit_parser/commit_parser/__init__.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 def django_parser(commit_msg):
     level, change_type, scope, content = parse_commit_message(commit_msg)
 
-    if "django" not in scope:
+    if not scope or "django" not in scope:
         return 0, "chore", scope, content
 
     return level, change_type, scope, content
