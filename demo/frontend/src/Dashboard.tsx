0c82d8be3fe299dfd8f773bab16c7e3765847125
Edward Lecock <edward.lecock@ambient-innovation.com>
2020-03-09 16:00:20 +0100

    chore(demo): add 401 button and fix material-ui/styles  dependency hell (refs #2)

--- demo/frontend/src/Dashboard.tsx
+++ demo/frontend/src/Dashboard.tsx
@@ -1,19 +1,67 @@
 import React, { FC, useContext } from 'react';
-import { Button } from '@material-ui/core';
+import {
+  Button, Grid, Typography,
+} from '@material-ui/core';
 import { UserContext } from 'ai-kit-auth';
 
 export const Dashboard: FC = () => {
-  const { logout } = useContext(UserContext);
+  const { logout, user } = useContext(UserContext);
+
+  const unAuthorizedFetch = () => {
+    fetch('http://localhost:8000/api/v1/unauthorized')
+      .then((response) => (response.json()))
+      .catch(logout);
+  };
 
   return (
-    <div>
-      Django Test App
-      <Button
-        variant="contained"
-        onClick={() => logout()}
-      >
-        Logout
-      </Button>
-    </div>
+    <Grid container alignItems="center" justify="center" spacing={2}>
+      <Grid item xs={12}>
+        <Typography
+          align="center"
+          variant="h1"
+        >
+          Django Test App
+        </Typography>
+
+        <Typography
+          align="center"
+          variant="body1"
+        >
+          { /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */ }
+          {`User: ${user!.username}`}
+        </Typography>
+
+        <Typography
+          align="center"
+          variant="body1"
+        >
+          { /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */ }
+          {`Email: ${user!.email}`}
+        </Typography>
+
+      </Grid>
+
+      <Grid container item xs={12} spacing={2} justify="center">
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => logout()}
+          >
+            Logout
+          </Button>
+        </Grid>
+
+        <Grid item>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => unAuthorizedFetch()}
+          >
+            Trigger 401
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
