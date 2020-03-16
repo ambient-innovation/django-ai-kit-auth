3b0f252b245c97607a32e0828b404fd6344885da
Matthias Herzkamp <matthias.herzkamp@ambient-innovation.com>
2020-03-16 15:49:41 +0100

    feat(react): show appropriate notification after auth logout (refs #2)

--- react-lib/src/store/types.ts
+++ react-lib/src/store/types.ts
@@ -4,13 +4,19 @@ export interface UserStoreValue<U = User> {
   user: U|null;
 }
 
+export enum LogoutReason {
+  NONE,
+  USER,
+  AUTH,
+}
+
 export interface AuthFunctionContextValue {
   apiUrl: string;
   csrf: string;
   loading: boolean;
   login: (userIdentifier: string, password: string) => Promise<void>;
   loggedIn: boolean;
-  logout: () => Promise<void>;
-  justLoggedOut: boolean;
+  logout: (reason?: LogoutReason) => Promise<void>;
+  justLoggedOut: LogoutReason;
   activateEmailAddress: (userIdentifier: string, token: string) => Promise<void>;
 }
