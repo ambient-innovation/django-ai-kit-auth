bfa627c14a4238246506557a295231e97dd52bbd
matthias.tellen@ambient-innovation.com <e1993DCnCPCq6MtpuJwb>
2020-03-17 16:13:00 +0100

    fix(django): routes  (refs #5)

--- django-app/ai_kit_auth/urls.py
+++ django-app/ai_kit_auth/urls.py
@@ -4,13 +4,9 @@ from . import views
 auth_patterns = (
     [
         re_path(r"^login/$", views.LoginView.as_view(), name="login"),
-        re_path(
-            r"^activate_email/(?P<ident>[^\/]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
-            views.ActivateUser.as_view(),
-            name="activate",
-        ),
+        re_path(r"^activate_email/$", views.ActivateUser.as_view(), name="activate",),
         re_path(r"^logout/$", views.LogoutView.as_view(), name="logout"),
-        re_path(r"^me/$", views.Me.as_view(), name="me"),
+        re_path(r"^me/$", views.MeView.as_view(), name="me"),
         re_path(
             r"^validate_password/$",
             views.ValidatePassword.as_view(),
