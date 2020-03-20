c0dfd34f85a95a27aea709d14127e92edf4b858f
matthias.tellen@ambient-innovation.com <e1993DCnCPCq6MtpuJwb>
2020-03-20 13:38:18 +0100

    fix(django): re_path -> path (refs #5)

--- django-app/ai_kit_auth/urls.py
+++ django-app/ai_kit_auth/urls.py
@@ -1,24 +1,24 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 from . import views
 
 auth_patterns = (
     [
-        re_path(r"^login/$", views.LoginView.as_view(), name="login"),
-        re_path(r"^activate_email/$", views.ActivateUser.as_view(), name="activate",),
-        re_path(r"^logout/$", views.LogoutView.as_view(), name="logout"),
-        re_path(r"^me/$", views.MeView.as_view(), name="me"),
-        re_path(
-            r"^validate_password/$",
+        path(r"login/", views.LoginView.as_view(), name="login"),
+        path(r"activate_email/", views.ActivateUser.as_view(), name="activate",),
+        path(r"logout/", views.LogoutView.as_view(), name="logout"),
+        path(r"me/", views.MeView.as_view(), name="me"),
+        path(
+            r"validate_password/",
             views.ValidatePassword.as_view(),
             name="validate_password",
         ),
-        re_path(
-            r"send_pw_reset_email/$",
+        path(
+            "send_pw_reset_email/",
             views.InitiatePasswordResetView.as_view(),
             name="send_pw_reset_email",
         ),
-        re_path(r"reset_password/$", views.ResetPassword.as_view(), name="pw_reset",),
-        re_path(r"^register/$", views.RegistrationView.as_view(), name="register"),
+        path(r"reset_password/", views.ResetPassword.as_view(), name="pw_reset",),
+        path(r"register/", views.RegistrationView.as_view(), name="register"),
     ],
     "ai_kit_auth",
 )
